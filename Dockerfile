3b67edb589990e041551dddbdf21788211a688f0
Eirik <eiriksletteberg@gmail.com>
2018-04-12 15:12:34 +0200

    Add Travis CI build
    This builds a Docker container which
    runs the github integration app.

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM node:9.11.1-alpine as build
+
+WORKDIR /app
+
+ADD . .
+
+RUN yarn --production=false
+
+RUN yarn lint
+
+RUN yarn test
+
+RUN yarn build
+
+FROM node:9.11.1-alpine
+
+WORKDIR /app
+
+COPY package.json yarn.lock /app/
+
+RUN yarn --production=true
+
+COPY --from=build /app/.next /app/.next
+
+COPY --from=build /app/.server-dist /app/.server-dist
+
+COPY static /app/static
+
+EXPOSE 3000
+
+CMD ["yarn", "start"]
--- server/config.ts
+++ server/config.ts
@@ -3,11 +3,15 @@ import fs from 'fs'
 import path from 'path'
 
 const envFile = process.env.CONFIG || path.resolve(process.cwd(), '.env')
-const parseResult = dotenv.config({ path: envFile })
-
-if (parseResult.error) {
-  console.error('Could not parse config', parseResult.error);
-  process.exit(1);
+if (fs.existsSync(envFile)) {
+  const parseResult = dotenv.config({ path: envFile })
+  if (parseResult.error) {
+    console.error('Could not parse config', parseResult.error)
+    process.exit(1)
+  }
+} else {
+  console.log(`No .env file detected at ${envFile}, will skip reading configuration from there.`)
 }
+
 
 export const getConfig = key => process.env[key]
