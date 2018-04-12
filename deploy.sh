39f68265972c2bfc0434f1da6a8b989d62c88e4f
Eirik <eiriksletteberg@gmail.com>
2018-04-12 16:43:35 +0200

    Fiks navn på Docker-image
    Deploy feilet fordi Docker-imaget hadde feil navn...

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "navikt/$TRAVIS_COMMIT"
+docker push "navikt/nav-aapen-kildekode:$TRAVIS_COMMIT"
